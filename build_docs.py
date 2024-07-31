eff0e245c58612a25553eeb97fa4e953342732ce
Dalena <mimidalena@gmail.com>
2024-07-31 11:41:23 -0500

    fix: path

--- build_docs.py
+++ build_docs.py
@@ -18,7 +18,8 @@ current_script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root_dir = os.path.abspath(os.path.join(current_script_dir, '..'))
 source_dir = os.path.join(project_root_dir, 'docs')
 
-DOCS_BUILD_PATH = os.path.join(project_root_dir, "docs/_build/ape_vyper")
+DOCS_BUILD = os.path.join(project_root_dir, "docs/_build/ape_vyper")
+DOCS_BUILD_PATH = PATH(f"{DOCS_BUILD}")
 LATEST_PATH = DOCS_BUILD_PATH / "latest"
 STABLE_PATH = DOCS_BUILD_PATH / "stable"
 
